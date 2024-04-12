9a858e890cc68e4cbc2b285d8790e616f6db1226
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-04-12 19:52:55 -0400

    binki-atlassian-jira-copy-issue-key-1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+Add a “Copy issue key” button next to the “copy link to issue” button which appears when you hover over the breadcrumbs.
+
+[Install](binki-atlassian-jira-copy-issue-key.user.js?raw=1)
---
+++ binki-atlassian-jira-copy-issue-key.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name     binki-atlassian-jira-copy-issue-key
+// @homepageURL https://github.com/binki/binki-atlassian-jira-copy-issue-key
+// @version  1.0.0
+// @match https://*.atlassian.net/*
+// @require https://github.com/binki/binki-userscript-when-element-changed-async/raw/88cf57674ab8fcaa0e86bdf5209342ec7780739a/binki-userscript-when-element-changed-async.js
+// ==/UserScript==
+
+(async () => {
+  const button = document.createElement('button');
+  button.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" role="presentation" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor" fill-rule="evenodd"><path d="M 12.476563,4 C 9.4606244,4 7,6.4606237 7,9.4765625 V 12.523438 C 7,15.539376 9.4606242,18 12.476563,18 h 3.046875 C 18.539377,18 21,15.539376 21,12.523438 V 9.4765625 C 21,6.4606238 18.539376,4 15.523438,4 Z m 0,2.0507812 h 3.046875 c 1.915184,0 3.425781,1.5105973 3.425781,3.4257813 v 3.0468755 c 0,1.915183 -1.510598,3.425781 -3.425781,3.425781 h -3.046875 c -1.915183,0 -3.4257817,-1.510599 -3.4257817,-3.425781 V 9.4765625 c 0,-1.9151838 1.5105987,-3.4257812 3.4257817,-3.4257813 z" /><path d="M 11.523438,18.949219 H 8.4765625 c -1.9151837,0 -3.4257813,-1.510597 -3.4257813,-3.425781 V 12.476563 L 3,12.476563 v 3.046875 C 3,18.539376 5.4606236,21 8.4765625,21 h 3.0468755" /></g></svg>';
+  button.title = 'Copy issue key';
+  button.addEventListener('click', e => {
+    navigator.clipboard.writeText(document.querySelector('[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]').textContent);
+    e.preventDefault();
+    button.disabled = true;
+    setTimeout(() => button.disabled = false, 1000);
+  });
+
+  while (true) {
+    await whenElementChangedAsync(document.body);
+    const keyPermalinkButton = document.querySelector('[data-testid="issue.common.component.permalink-button.button.copy-link-button-wrapper"] button');
+    if (!keyPermalinkButton) continue;
+    button.className = keyPermalinkButton.className;
+    keyPermalinkButton.parentElement.parentElement.append(button);
+  }
+})();
